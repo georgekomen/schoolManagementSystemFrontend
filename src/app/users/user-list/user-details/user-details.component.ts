c23bce2a2ce6f8a25103e22ae2b435c20ac249a2
KOMEN <g.komen@carepay.co.ke>
2018-09-27 23:13:38 +0300

    can add school

--- src/app/users/user-list/user-details/user-details.component.ts
+++ src/app/users/user-list/user-details/user-details.component.ts
@@ -5,6 +5,8 @@ import {User} from '../../../shared/Models/user';
 import {SchoolControllerComponent} from '../../../school/school-list/school-controller/school-controller.component';
 import {MatDialogRef} from '@angular/material';
 import {Identification} from '../../../shared/Models/Identification';
+import {SchoolDataSource} from '../../../school/school-list/school-list-datasource';
+import {School} from '../../../shared/Models/school';
 
 @Component({
   selector: 'app-user-details',
@@ -13,6 +15,7 @@ import {Identification} from '../../../shared/Models/Identification';
 })
 export class UserDetailsComponent implements OnInit {
   public user: User = new User();
+  schoolList: School[] = [];
 
   constructor(private schoolService: SchoolService,
               private userService: UserService,
@@ -23,9 +26,16 @@ export class UserDetailsComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getSchoolList();
   }
 
-  postSchool() {
+  getSchoolList() {
+    this.schoolService.getSchools().subscribe(res => {
+      this.schoolList = res;
+    });
+  }
+
+  postUser() {
     this.userService.postUser(this.user).subscribe(res => {
       console.log(res);
       this.closeDialog();
